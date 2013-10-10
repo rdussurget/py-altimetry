172870d00a52588b94888706db4eee280c5ae928
Renaud Dussurget <rdussurget@cls.fr>
2013-10-10 16:08:04 +0200

    RD - distribution is now fine waiting for upload on pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='R. Dussurget',
     author_email='renaud.dussurget@gmail.com',
     packages=['altimetry','altimetry.config','altimetry.data','altimetry.tools','altimetry.externals'],
-    scripts=['bin/cdf_merge.py','bin/test_spectral_analysis.py','altimetry/examples/spectral_tapering.py','altimetry/examples/j2_spectrum.py'],
+    scripts=['bin/cdf_merge.py','altimetry/examples/spectral_tapering.py','altimetry/examples/j2_spectrum.py'],
     url='https://code.google.com/p/py-altiwaves/',
     license='LICENSE.TXT',
     description='PyAltimetry: Python tools for altimetry data and much more',
